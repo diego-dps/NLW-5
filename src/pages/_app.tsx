7307ae9a813bda8f4921df835ec88139dce8cdf2
Diego Pereira Silva <diego.pereira2806@hotmail.com>
2021-04-25 23:52:14 -0300

    Criação da pagina Home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,11 @@
 import '../styles/global.scss';
-import '../styles/app.module.scss';
+import styles from '../styles/app.module.scss';
 import { Header } from '../components/Header';
 import { Player } from '../components/Player';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
+    <div className={styles.wrapper}>
       <main>
         <Header />
         <Component {...pageProps} />
